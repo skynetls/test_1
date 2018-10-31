52be5d826aae8c59729679fb119cc6929f30efc5
skynetls <lyudocheksi@gmail.com>
2018-10-31 19:20:52 +0300

    task_5 v2

---
+++ src/main/java/task_5_v2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task_5_v2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение в двоичной системе счисления: ");
+        String X = scanner.next();
+
+        int b = Integer.parseInt(X, 2);
+
+        System.out.println("Перевод в десятичную систему счисления: " + b);
+
+        scanner.close();
+    }
+}
+
